00d6f004967b0fc000b22e01aecb387d0ab16b88
Sylvain Bougerel <sylvain.bougerel@gmail.com>
2023-04-22 15:15:45 +0800

    Added basic logic to pull and push
    Pull on file load, pulls on file save.

--- auto-git-sync.el
+++ auto-git-sync.el
@@ -41,7 +41,8 @@
 
 ;;; Code:
 
-(require 'magit)
+(require 'magit-git)
+(require 'magit-process)
 
 (defgroup auto-git-sync nil
   "Automated git synchronisation with upstream."
@@ -54,9 +55,44 @@
   :group 'auto-git-sync)
 
 (defcustom auto-git-sync-dirs nil
-  "Alist of repositories that should be synchronised."
-  :type '(repeat (string :tag "Abolute path of repository to synchronise"))
+  "Alist of (DIR . MESSAGE) that should be synchronised.
+
+DIR is the top-level directory of the repository to synchronise.
+MESSAGE is the commit message to use when committing changes."
+  :type '(alist
+          :key-type (directory :tag "Top-level directory")
+          :value-type (string :tag "Commit message"))
   :group 'auto-git-sync)
+
+(defun auto-git-sync--sync-cons ()
+  "Return '(top-level-dir . message)' for repositories to synchronise or nil."
+  (let ((git-dir (magit-toplevel)))
+    (and git-dir
+         (assoc git-dir auto-git-sync-dirs))))
+
+(defun auto-git-sync--git-pull ()
+  "Execute `git pull`."
+  (when (auto-git-sync--sync-cons)
+    (run-hooks 'magit-credential-hook)
+    (magit-run-git-async "pull")))
+
+(defun auto-git-sync--git-push ()
+  "Execute `git push`."
+  (let ((sync-cons (auto-git-sync--sync-cons)))
+    (when sync-cons
+      (magit-run-git-async "commit" "-a" "-m" (cdr sync-cons))
+      (run-hooks 'magit-credential-hook)
+      (magit-run-git-async "push"))))
+
+(define-minor-mode auto-git-sync-mode
+  "Automatically sync buffer with Git repository."
+  :lighter " ↕"
+  (if auto-git-sync-mode
+      (progn
+        (add-hook 'after-save-hook #'auto-git-sync--git-push nil t)
+        (add-hook 'find-file-hook #'auto-git-sync--git-pull nil t))
+    (remove-hook 'after-save-hook #'auto-git-sync--git-push t)
+    (remove-hook 'find-file-hook #'auto-git-sync--git-pull t)))
 
 (provide 'auto-git-sync)
 ;;; auto-git-sync.el ends here
